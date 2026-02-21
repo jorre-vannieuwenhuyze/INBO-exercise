404070b535a54e95ffe043911df151a8b5051825
Jorre Vannieuwenhuyze <jorre.vannieuwenhuyze@vlaanderen.be>
2026-02-21 16:46:46 +0100

    verion 1

--- script.R
+++ script.R
@@ -18,3 +18,28 @@ data |>
 tmp <- data |>
   pivot_wider(id_cols='maand',names_from='AquaComponent',values_from='mean') |>
   GGally::ggpairs()
+
+
+set.seed(3454)
+simdata <- "simulated_oxygen_duplo.csv" |>
+  read_csv() |>
+  mutate(
+    Oxygen_mg_L=Oxygen_mg_L+runif(n(),-.5,.5),
+    add=runif(n(),-2,2),
+    add=add-mean(add),
+    Oxygen_mg_L2=Oxygen_mg_L+add,
+    ) 
+simdata |>
+  ggplot(aes(x=Month,y=Oxygen_mg_L)) +
+  geom_point()
+simdata |>
+  ggplot(aes(x=Month,y=Oxygen_mg_L2)) +
+  geom_point()
+
+simdata |>
+  mutate(code=sprintf("%i/%.4f",Month,Oxygen_mg_L2)) |>
+  summarize(code=str_c(code,collapse=",")) |>
+  pull(code) |>
+  cat()
+simdata$Oxygen_mg_L2 |> mean()
+
