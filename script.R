3088aebf81773ede531ed3bb60f7d507189214af
Jorre Vannieuwenhuyze <jorre.vannieuwenhuyze@vlaanderen.be>
2026-02-22 13:17:18 +0100

    Check

--- script.R
+++ script.R
@@ -10,12 +10,12 @@ data |>
   facet_wrap(~AquaComponent,scales="free_y")
 
 data |>
-  ggplot(aes(x=maand,y=max)) +
+  ggplot(aes(x=maand,y=mean)) +
   geom_line() +
   facet_wrap(~AquaComponent,scales="free_y")
 
 
-tmp <- data |>
+data |>
   pivot_wider(id_cols='maand',names_from='AquaComponent',values_from='mean') |>
   GGally::ggpairs()
 
@@ -29,6 +29,12 @@ simdata <- "simulated_oxygen_duplo.csv" |>
     add=add-mean(add),
     Oxygen_mg_L2=Oxygen_mg_L+add,
     ) 
+simdata_agg <- simdata |>
+  summarise(
+    Oxygen_mg_L=mean(Oxygen_mg_L),
+    Oxygen_mg_L2=mean(Oxygen_mg_L2),
+    .by=Month
+    )
 simdata |>
   ggplot(aes(x=Month,y=Oxygen_mg_L)) +
   geom_point()
@@ -42,4 +48,40 @@ simdata |>
   pull(code) |>
   cat()
 simdata$Oxygen_mg_L2 |> mean()
+simdata_agg |>
+  mutate(code=sprintf("(%i,%.4f)",Month,Oxygen_mg_L2)) |>
+  summarize(code=str_c(code,collapse=" -- ")) |>
+  pull(code) |>
+  cat()
+
+
+
+set.seed(54613)
+ACdata <- data |>
+  filter(AquaComponent=="O2verz") |>
+  mutate(
+    add=runif(n(),-50,50),
+    add=add-mean(add),
+    mean2=mean+add,
+    )
+ggplot(ACdata,aes(x=maand)) +
+  geom_line(aes(y=mean)) +
+  geom_line(aes(y=mean2),color="red") +
+  ylim(0,180)
+mean(ACdata$mean)
+ACdata |>
+  mutate(code=sprintf("(%i,%.4f) circle(1pt)",maand,mean2)) |>
+  summarize(code=str_c(code,collapse=" -- ")) |>
+  pull(code) |>
+  cat()
+
+
+corrdata <- data |>
+  filter(AquaComponent %in% c("O2verz","pH_veld")) |>
+  pivot_wider(id_cols = maand, names_from = AquaComponent, values_from = mean)
+corrdata |>
+  mutate(code=sprintf("%.4f/%.4f",O2verz,pH_veld)) |>
+  summarize(code=str_c(code,collapse=",")) |>
+  pull(code) |>
+  cat()
 
